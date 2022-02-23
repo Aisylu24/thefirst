efc3f05e0e7c74ea59b0f5136f104cff563ee5b8
Aisylu <firnatovna@mail.ru>
2022-02-23 22:33:14 +0300

    2

---
+++ js/codewars.js
@@ -0,0 +1,19 @@
+﻿// Convert a Number to a String!
+  
+function numberToString(num) {
+    return (num).toString(); 
+      
+    }
+
+
+ // Find the Remainder
+
+ function remainder(n, m){
+    if(n > m){
+      return n % m
+    }  else {
+      return m % n
+    }
+    }
+
+
