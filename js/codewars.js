a7f4c2cc3388e2f158597adc1364114bd011370d
Aisylu24 <firnatovna@mail.ru>
2022-02-24 21:39:59 +0300

    2402

--- js/codewars.js
+++ js/codewars.js
@@ -21,3 +21,13 @@ function numberToString(num) {
 >>>>>>> 0f2b244404025e5c1ce21c65ed735ed6b0ed2c5f
 
 
+// Pirates!! Are the Cannons ready!??
+
+const cannonsReady = (gunners) => {
+  if (Object.values(gunners).includes('nay')) {
+    return "Shiver me timbers!"   } 
+  else { 
+ return 'Fire!'
+       } 
+}
+
