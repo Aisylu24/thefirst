4f84ce8f3d22393e70ac6b602bd24a4259bacf42
Aisylu24 <firnatovna@mail.ru>
2022-02-25 19:42:12 +0300

    Update fcc.js

--- js/fcc.js
+++ js/fcc.js
@@ -2,3 +2,18 @@ const myStr = "I am a \"double quoted\" string inside \"double quotes\"."; // Ch
 //
 const myStr = '<a href="http://www.example.com" target="_blank">Link</a>';
 //
+const myStr ="FirstLine\n\t\\SecondLine\nThirdLine"
+//
+const myStr = "This is the start. " + "This is the end.";
+//
+let myStr = "This is the first sentence. ";
+myStr+="This is the second sentence." 
+
+//
+const myName = "Ais";
+const myStr = "Hello " + myName + "what's up";
+
+//
+const someAdjective = "stunning";
+let myStr = "Learning to code is ";
+myStr += someAdjective 
