f1696b8767928bc29783191676cbc9379547b3f5
Aisylu24 <firnatovna@mail.ru>
2022-02-27 21:27:17 +0300

    2702.

--- js/fcc.js
+++ js/fcc.js
@@ -41,3 +41,39 @@ let myStr = "Jello World";
 // Only change code below this line
 myStr = "Hello World"; // Change this line
 // Only change code above this line
+
+
+//
+
+// Setup
+const lastName = "Lovelace";
+
+// Only change code below this line
+const thirdLetterOfLastName = lastName[2]; // Change this line
+
+
+//
+
+const lastName = "Lovelace";
+
+// Only change code below this line
+const lastLetterOfLastName = lastName[lastName.length -1]; // Change this line
+
+//
+const lastName = "Lovelace";
+
+// Only change code below this line
+const secondToLastLetterOfLastName = lastName[lastName.length-2]; // Change this line
+
+//
+
+const myNoun = "dog";
+const myAdjective = "big";
+const myVerb = "ran";
+const myAdverb = "quickly";
+
+// Only change code below this line
+const wordBlanks =  "this " + myAdjective + " " + myNoun + " " + myVerb + " " + myAdverb + " around a house" + "."; // Change this line
+// Only change code above this line
+
+console.log(wordBlanks)
