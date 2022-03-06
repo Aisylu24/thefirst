a567845e5f6083ffd193f2bc8a57a08979bd664f
Aisylu24 <firnatovna@mail.ru>
2022-03-06 17:52:03 +0300

    0603

--- js/fcc.js
+++ js/fcc.js
@@ -93,7 +93,7 @@ myArray.push(["dog", 3])
 //
 const myArray = [["John", 23], ["cat", 2]];
 const removedFromMyArray = myArray.pop()
-
+console.log(removedFromMyArray);
 //
 
 const myArray = [["John", 23], ["dog", 3]];
@@ -106,3 +106,154 @@ console.log(myArray);
 const myArray = [["John", 23], ["dog", 3]];
 myArray.shift();
 myArray.unshift(["Paul", 35])
+
+
+//
+
+const myList = [['aisylu', 25],['aisylu', 25],['aisylu', 25],['aisylu', 25],['aisylu', 25]];
+
+
+//
+
+function reusableFunction() {
+  console.log("Hi World"); 
+  }
+
+  reusableFunction()
+
+
+  // 0603 1
+
+  function functionWithArgs(a,b) {
+    console.log(a+b)
+  }
+  
+  functionWithArgs(1,2)
+  functionWithArgs(7,9)
+
+
+  // 2
+
+
+  function timesFive(num) 
+{ return num *5 }
+
+
+timesFive(5) 
+
+timesFive(2)
+
+timesFive(0) 
+
+
+// 3
+
+
+let myGlobal = 10
+let oopsGlobal = 5
+function fun1() {
+oopsGlobal
+  
+}
+
+// Only change code above this line
+
+function fun2() {
+  var output = "";
+  if (typeof myGlobal != "undefined") {
+    output += "myGlobal: " + myGlobal;
+  }
+  if (typeof oopsGlobal != "undefined") {
+    output += " oopsGlobal: " + oopsGlobal;
+  }
+  console.log(output);
+}
+
+
+// 4
+
+function myLocalScope() {
+  
+let myVar
+  console.log('inside myLocalScope', myVar);
+}
+myLocalScope();
+
+console.log('outside myLocalScope', myVar);
+
+
+// 5
+
+const outerWear = "T-Shirt";
+
+function myOutfit() {
+  // Only change code below this line
+let outerWear = 'sweater'
+  // Only change code above this line
+  return outerWear;
+}
+
+myOutfit();
+
+
+// 6
+
+let sum = 0;
+function addFive() {
+ sum = sum + 5
+}
+function addThree() {
+   sum = sum + 3;
+}
+
+addFive()
+addThree()
+
+
+
+// 7
+
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+processed = processArg(7)
+
+
+// 8
+
+function nextInLine(arr, item) {
+  arr.push(item)
+  return arr.shift();
+    
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
+
+  
+  // 9
+
+  function welcomeToBooleans() {
+    return true;
+    
+
+// 10
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+if(wasThatTrue) {
+  return "Yes, that was true" 
+  }
+  return "No, that was false"
+}
+
+
+
