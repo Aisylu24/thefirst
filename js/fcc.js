f9eae120e5e1d0f56a918ce7ceadac254e683c48
Aisylu24 <firnatovna@mail.ru>
2022-03-08 21:30:34 +0300

    0803

--- js/fcc.js
+++ js/fcc.js
@@ -475,4 +475,217 @@ function testSize(num) {
  
  }
  
- testSize(7);
+ // testSize(7);
+
+
+// 0803 1 
+
+const golfNames = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"]
+
+function golfScore(par, strokes) {
+
+  if (strokes == 1)  { return golfNames[0]}
+ else if (strokes <= par - 2){ return golfNames[1]}
+ else if (strokes == par - 1) {return golfNames[2]}
+ else if (strokes == par ) {return golfNames[3]}
+  else if (strokes == par + 1) {return golfNames[4]}
+   else if (strokes == par + 2) {return golfNames[5]}
+   else(strokes >= par + 3)
+   return golfNames[6]
+
+}
+
+console.log(golfScore(5, 4))
+
+//2
+
+function caseInSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+  switch(val) { case 1:
+    answer = "alpha"
+    break;
+  case 2:
+    answer = "beta"
+    break;
+  
+    case 3:
+    answer="gamma"
+    break;
+    case 4:
+    answer= "delta"
+    break;}
+
+  // Only change code above this line
+  return answer;
+}
+
+caseInSwitch(1);
+
+
+//3
+
+function switchOfStuff(val) {
+  let answer = "";
+  // Only change code below this line
+
+switch (val) {
+  case "a": answer = "apple";
+  break;
+  case "b": answer = "bird"; 
+  break;
+  case "c": answer = "cat";
+  break;
+
+  default: answer = "stuff";
+break; }
+
+  // Only change code above this line
+  return answer;
+}
+
+switchOfStuff(1);
+
+
+// 4
+
+function sequentialSizes(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val)
+{
+case 1:
+case 2:
+case 3:
+answer = "Low";
+break;
+case 4: 
+case 5:
+case 6:
+answer = "Mid";
+break;
+case 7: 
+case 8:
+case 9:
+answer = "High";
+break;
+}
+  // Only change code above this line
+  return answer;
+}
+sequentialSizes(1);
+
+
+//5 
+
+function chainToSwitch(val) {
+  let answer = "";
+  // Only change code below this line
+switch(val) {
+  case "bob":
+    answer = "Marley";
+    break;
+  case 42 :
+    answer = "The Answer"; break;
+ case 1:
+    answer = "There is no #1"; break;
+  case 99: 
+    answer = "Missed me by this much!"; break;
+ case 7: 
+    answer = "Ate Nine"; break;
+  }
+
+  // Only change code above this line
+  return answer;
+}
+
+chainToSwitch(7);
+
+
+//6
+
+function isLess(a, b) {
+  return a < b
+ }
+ 
+ isLess(10, 15);
+
+ //7 
+
+ function abTest(a, b) {
+ 
+  if (a<0 || b<0)
+  return undefined
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+  }
+  abTest(2,2);
+
+// 8 
+
+
+let count = 0;
+function cc(card) {
+  
+switch(card) {
+case 2: 
+case 3:
+case 4:
+case 5:
+case 6:
+count++;
+break;
+case 10: 
+case 'J':
+case 'Q':
+case 'K':
+case 'A':
+count--;
+break;
+}
+
+let holdbet = "Hold"
+if (count >0) {holdbet = "Bet"}
+
+return count + " " + holdbet;
+
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+//9 
+
+let myDog = {
+name: "Dog",
+tails: 1,
+legs: 4,
+friends: ["people", "another dogs"]
+};
+
+
+// 10
+
+const testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+const hatValue = testObj.hat     
+const shirtValue = testObj.shirt  
+
+
+// 11
+
+const testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+const entreeValue = testObj["an entree"]  
+const drinkValue = testObj["the drink"]   
+
+
+
+//12
+
