02616d1cd8069d22baef12e7337ca6a5e934ffc7
Aisylu24 <firnatovna@mail.ru>
2022-03-20 15:50:25 +0300

    2003

--- js/codewars.js
+++ js/codewars.js
@@ -358,3 +358,16 @@ function defineSuit(card) {
       }
       return str;
     }
+
+
+
+    //
+
+
+    function lovefunc(flower1, flower2){
+      if ((flower1+flower2)%2 == 0)
+     return false
+     else return true
+                
+       // moment of truth
+     }
